c99227e1d2445928cac610fc5224a039554393cf
JrunDing <895536196@qq.com>
2023-08-30 00:49:21 +0800

    update

---
+++ normalization.py
@@ -0,0 +1,43 @@
+"""
+@Author: Jrun Ding
+@Date: 2023.8.29
+@Brief: 批量归一化
+@Coding: utf-8
+"""
+
+import torch
+from torch import nn
+from d2l import torch as d2l
+
+
+net = nn.Sequential()
+net.add(nn.Conv2D(6, kernel_size=5),
+        nn.BatchNorm(),  # Batch Normalization
+        nn.Activation('sigmoid'),
+        nn.AvgPool2D(pool_size=2, strides=2),
+        nn.Conv2D(16, kernel_size=5),
+        nn.BatchNorm(),
+        nn.Activation('sigmoid'),
+        nn.AvgPool2D(pool_size=2, strides=2),
+        nn.Dense(120),
+        nn.BatchNorm(),
+        nn.Activation('sigmoid'),
+        nn.Dense(84),
+        nn.BatchNorm(),
+        nn.Activation('sigmoid'),
+        nn.Dense(10)
+        )
+
+
+net = nn.Sequential(
+    nn.Conv2d(1, 6, kernel_size=5), nn.BatchNorm2d(6), nn.Sigmoid(),
+    nn.AvgPool2d(kernel_size=2, stride=2),
+    nn.Conv2d(6, 16, kernel_size=5), nn.BatchNorm2d(16), nn.Sigmoid(),
+    nn.AvgPool2d(kernel_size=2, stride=2), nn.Flatten(),
+    nn.Linear(256, 120), nn.BatchNorm1d(120), nn.Sigmoid(),
+    nn.Linear(120, 84), nn.BatchNorm1d(84), nn.Sigmoid(),
+    nn.Linear(84, 10)
+)
+
+
+
